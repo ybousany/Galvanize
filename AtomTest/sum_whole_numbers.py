dbcf9c60c416d0ab83fcd12c43dd7af81263ee3a
Yonit Bousany <yonit.bousany@boha81788x.admin.bvsd.k12.co.us>
2017-04-11 12:56:27 -0600

    Added warm-up 2 to Week2Day1HW.py

--- AtomTest/sum_whole_numbers.py
+++
@@ -1,7 +0,0 @@
-my_num = int(raw_input("Enter a number to sum up to: "))
-sum_result = 0
-current = 1
-while current <= my_num:
-    sum_result += current
-    current += 1
-print(sum_result)
--- Week2Day1HW.py
+++ Week2Day1HW.py
@@ -1,8 +1,20 @@
-user_input = raw_input('String to add to end of my_str: ')
-my_str = "This String wasn't Chosen Arbitrarily... "
+#WARM-UP 1
+
+#user_input = raw_input('String to add to end of my_str: ')
+#my_str = "This String wasn't Chosen Arbitrarily... "
 
 
-if len(user_input) < 10:
-    my_str += user_input
+#if len(user_input) < 10:
+#    my_str += user_input
 
-print my_str
+#print my_str
+
+#WARM-UP 2
+
+my_list = [1, 'hello', 2, 'there', 3, 'list']
+user_input= raw_input("Enter something for my_list: ")
+if len(user_input) < 8:
+    my_list.append(user_input)
+
+else: my_list.append(4)
+print my_list
